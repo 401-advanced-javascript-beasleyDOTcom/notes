7a4831aaa923f5ef19377df90814afaca32d3a47
Beasleydotcom <beasley.com@gmail.com>
2020-08-10 20:47:33 -0700

    got it the way I want it

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ const Input = require('./lib/input.js');
 const notes = require('./lib/notes.js');
 const options = new Input();
 console.log(options)
-console.log(options.action)
-console.log(options.payload)
+// console.log(options.action)
+// console.log(options.payload)
 // notes.fetch(options);
