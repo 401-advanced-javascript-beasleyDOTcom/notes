eebf0568e115028b6eb96d77fc0c1b961ed5108f
Beasleydotcom <beasley.com@gmail.com>
2020-08-10 21:44:46 -0700

    got it goooin onnnn thanks joseph!

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 'use strict'
 
 const Input = require('./lib/input.js');
-const notes = require('./lib/notes.js');
+const Notes = require('./lib/notes.js');
 const options = new Input();
+const payload = new Notes(options);
 console.log(options)
+console.log('this is the payload', payload)
 // console.log(options.action)
 // console.log(options.payload)
 // notes.fetch(options);
