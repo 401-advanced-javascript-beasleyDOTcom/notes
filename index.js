a58125988915f178d589b6456b5821c9812e94e5
Beasleydotcom <beasley.com@gmail.com>
2020-08-11 23:47:14 -0700

    got stuff running but still need to make more tests for ntoes and input

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
-const options = new Input();
-const payload = new Notes(options);
-console.log(options)
-console.log('this is the payload', payload)
+const input = new Input();
+const notes = new Notes(input);
+console.log('this is your input', input)
+console.log('this is the note', notes)
 // console.log(options.action)
 // console.log(options.payload)
 // notes.fetch(options);
