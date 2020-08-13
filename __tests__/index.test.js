7bf9f80e2af7bc1fef773e9f870caaf5fcb97138
Beasleydotcom <beasley.com@gmail.com>
2020-08-12 22:08:07 -0700

    got really far

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,7 +11,7 @@ minimist.mockImplementation(() => {
 
 const Input = require('../lib/input.js');
 
-describe('Input Module', () => {
+describe('Parse ON!', () => {
 
     it('parse() creates a good object', () => {
         let options = new Input();
@@ -20,7 +20,31 @@ describe('Input Module', () => {
         expect(command.action).toBe('add');
         expect(command.payload).toBe('test');
     });
+    it('should parse --add and payload', () => {
+        const input = new Input();
+        const command = input.parse({add: 'what a good payload!'});
+        expect(command.action).toBe('add');
+        expect(command.payload).toBe('what a good payload!');
+    });
 
+    it('should have undefined action and payload for unknown sWitches', () => {
+        const input = new Input();
+        const command = input.parse({ unknown: 'some paylord'});
+        expect(command.action).not.toBeDefined();
+        expect(command.payload).not.toBeDefined();      
+    });
+
+describe('VALIDATE!', () =>{
+    it('valid() is totally cool with a proper object', () => {
+        let options = new Input();
+        expect(options.valid()).toBe(true);
+    });
+    it('valid() should be completely uncool with an invalid object', () => {
+        let options = new Input();
+        options.command = {};
+        expect(options.valid()).toBe(false);
+    });
+})
     it('valid() respects a proper object', () => {
         let options = new Input();
         expect(options.valid()).toBeTruthy();
@@ -31,5 +55,32 @@ describe('Input Module', () => {
         options.command = {}; // break it
         expect(options.valid()).toBeFalsy();
     });
+    //stil workign on below test
+    describe('parse category', () => {
+        it('parse -a with payload and --category', () => {
+            let options = new Input();
+            let command = options.parse({ a: 'test' });
+            console.log('this is command in test', command)
+            expect(command.action).toBe('add');
+            expect(command.payload).toBe('test');
+        });
 
+    })
+    describe('category', () =>{
+        it('should parse category with full switch,', () => {
+            let options = new Input();
+            const actual = options.parse({ add: 'evict frog tennant', category: 'things to do'});
+            expect(actual.category).toBe('things to do');
+        });
+        it('should parse category with short switch', () => {
+            let options = new Input();
+            const actual = options.parse({add: 'think of blue skies and big storms', c: 'things to do'});
+            expect(actual.category).toBe('things to do');
+        });
+        it('should parse undefined category with missing switch', () => {
+            let options = new Input();
+            const actual = options.parse({add: 'get up! get on the scene!'});
+            expect(actual.category).not.toBeDefined();
+        });
+    })
 });
--- index.js
+++ index.js
@@ -1,10 +1,31 @@
 #!/usr/bin/env node
 'use strict'
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/notesy',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+});
+
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
+
 const input = new Input();
 const notes = new Notes(input);
+
+if(input.valid()){
+    notes.execute(input.command)
+        .then(mongoose.disconnect)
+            .catch(error => console.error('there has been an error!', error));
+    
+} else {
+    helpMe();
+}
+function helpMe(){
+    console.error('Error!');
+    process.exit();
+}
 console.log('this is your input', input)
 console.log('this is the note', notes)
 // console.log(options.action)
